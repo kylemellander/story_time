4309ccb231240db2be9a5fe842e1fa519f3dd837
Jason Lane & Janice Laset-Parkerson <student@epicodus.com>
2015-10-14 17:05:17 -0700

    Remove costum.js contents

--- app/assets/javascripts/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.fa').click(function(){
-    alert('hi');
-    $().append(<%= fa_icon "#{@images[rand(6)]}" %>)
-    $('#form').append('<%= j render("form") %>')
-  });
-});
